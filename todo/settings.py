d28ef8961e5c1fb50e814e965bf5516f67d3c730
harshlet <40534244+harshlet@users.noreply.github.com>
2021-03-22 20:51:56 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -121,5 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT =  Path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    Path.join(BASE_DIR,'static')
+]
